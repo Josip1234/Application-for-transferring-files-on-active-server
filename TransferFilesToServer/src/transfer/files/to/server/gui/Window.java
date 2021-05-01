c6d57815a56da7040c8c323516149b7324fa5ffd
Josip Bošnjak <jbosnjak@unipu.hr>
2021-05-01 14:30:23 +0200

    fixed big button bug

--- TransferFilesToServer/src/transfer/files/to/server/gui/Window.java
+++ TransferFilesToServer/src/transfer/files/to/server/gui/Window.java
@@ -2,6 +2,7 @@ package transfer.files.to.server.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -19,6 +20,7 @@ public class Window {
     /****
      * Add window with default 800x600 dimesion, with default close operation.
      * Default minimum size of frame window is 500x500, maximum 800x600.
+     * Flow layout is added to prevent big button to show on jframe.
      * Add button from buttons class to the jframe.
      */
 	public static void createFrame() {
@@ -29,6 +31,7 @@ public class Window {
 		//JLabel label = new JLabel("New label");
 		//label.setPreferredSize(new Dimension(width,height));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new FlowLayout());
 		frame.getContentPane().add(button,BorderLayout.CENTER);
 		frame.pack();
 		frame.setVisible(true);
