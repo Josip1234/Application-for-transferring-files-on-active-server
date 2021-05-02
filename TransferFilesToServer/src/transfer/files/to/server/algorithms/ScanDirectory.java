dd27ffc49c5f8b5ae3c8037d14f8f302dda51ad9
Josip Bošnjak <jbosnjak@unipu.hr>
2021-05-02 14:39:41 +0200

    added function for scanning directories

--- TransferFilesToServer/src/transfer/files/to/server/algorithms/ScanDirectory.java
+++ TransferFilesToServer/src/transfer/files/to/server/algorithms/ScanDirectory.java
@@ -1,7 +1,13 @@
 package transfer.files.to.server.algorithms;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /***
  * 
@@ -11,10 +17,25 @@ import java.util.List;
  *
  */
 public class ScanDirectory {
-	
-	public List<String> returnFilePathsFromSource(String directoryToScan){
+	/***
+	 * Here, we've populated a Stream using the .walk() method, passing a Paths argument. The Paths class consists of static methods that return a Path based on a String URI - and using a Path, we can locate the file easily.
+       The Path, Paths, Files, and many other classes belong to the java.nio package, which was introduced in Java 7 as a more modern way to represent files in a non-blocking way.
+	 * @param directoryToScan
+	 * @return list of files in directory
+	 */
+	public List<String> returnFilePathsFromSource(String directoryToScan) {
 		List<String> files = new ArrayList<String>();
+		try {
+			Stream<Path> path = Files.walk(Paths.get(directoryToScan));
+			List<String> result = path.filter(Files::isRegularFile).map(x -> x.toString()).collect(Collectors.toList());
+			files=result;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		return files;
+		
 	}
 
 }
