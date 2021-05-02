33af18f94499a4ba2be6306881974fc1a4b6f966
Josip Bošnjak <jbosnjak@unipu.hr>
2021-05-02 14:18:54 +0200

    added class for directory scan

---
+++ TransferFilesToServer/src/transfer/files/to/server/algorithms/ScanDirectory.java
@@ -0,0 +1,20 @@
+package transfer.files.to.server.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * 
+ * @author Josip Bošnjak
+ * This class contains algorithm which will scan all the files in the directory path. 
+ * It will return list of files, not file names, already file paths with file name and extensions.
+ *
+ */
+public class ScanDirectory {
+	
+	public List<String> returnFilePathsFromSource(String directoryToScan){
+		List<String> files = new ArrayList<String>();
+		return files;
+	}
+
+}
