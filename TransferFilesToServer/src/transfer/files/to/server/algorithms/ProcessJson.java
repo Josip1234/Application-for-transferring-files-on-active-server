74f18bd7bee1af0c8b6ad9c66c7d99a46af76818
Josip Bošnjak <jbosnjak@unipu.hr>
2021-05-06 20:15:50 +0200

    added class for json processing

---
+++ TransferFilesToServer/src/transfer/files/to/server/algorithms/ProcessJson.java
@@ -0,0 +1,14 @@
+package transfer.files.to.server.algorithms;
+/***
+ * 
+ * @author Josip Bošnjak
+ * This contains destination server validation.
+ * Files will not be copied or deleted until there is validated path of 
+ * destination folder found in json file. This is security feature to 
+ * prevent user accidently delete all files in folder which is 
+ * not on server.
+ *
+ */
+public class ProcessJson {
+
+}
