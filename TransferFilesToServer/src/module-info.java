a26080c795f3cf4f232e41733e97839db634472c
Josip Bošnjak <jbosnjak@unipu.hr>
2021-05-06 22:45:11 +0200

    fixed some errors

--- TransferFilesToServer/src/module-info.java
+++ TransferFilesToServer/src/module-info.java
@@ -2,7 +2,7 @@ module transferFilesToServer {
 	requires java.desktop;
 	requires javafx.graphics;
 	requires org.apache.commons.io;
-	requires com.fasterxml.jackson.databind;
+	
 	
 
 }
--- TransferFilesToServer/src/transfer/files/to/server/main/Main.java
+++ TransferFilesToServer/src/transfer/files/to/server/main/Main.java
@@ -1,13 +1,12 @@
 package transfer.files.to.server.main;
 
-import transfer.files.to.server.algorithms.ProcessJson;
 import transfer.files.to.server.gui.Window;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//Window.createFrame();
-		ProcessJson.returnListOfKeyValuesFromJson();
+		Window.createFrame();
+		
 
 	}
 
